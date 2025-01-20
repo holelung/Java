511097dc067b5d95271d16e6915e269dac043453
holelung <junho335276@gmail.com>
2025-01-20 11:21:31 +0900

    ignore

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,30 @@
+// JS와 주석 동일
+
+/* Run 버튼 ( 단축키  )
+ * 1) 자동으로 컴파일 수행(javac ooo.java)
+ * 	-> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행(java class 파일명)
+ * 
+ * */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이클립스 실행중");
+		System.out.println("자꾸 그러지마...");
+		System.out.println("타자 연습좀 해!!");
+		System.out.println("응이!");
+		
+		/* Java API DOC : 
+		 * Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서 
+		 * 
+		 * DOC 확인 방법
+		 * 1) java 21 api 검색
+		 * 
+		 * 2)
+		 * */
+	}
+
+}
